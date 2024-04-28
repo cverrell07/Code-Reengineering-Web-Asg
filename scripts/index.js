6466a0623a1275abaa539b303151dd887ca68e2f
Christopher Verrell <c.verrell07@gmail.com>
2024-04-28 17:06:46 +0700

    feat: add illustration and finisihing responsive

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+function scrollToContents() {
+    const contents = document.querySelector(".contents");
+    const windowHeight = window.innerHeight;
+    const contentsHeight = contents.offsetHeight;
+    const scrollPosition = contents.offsetTop - (windowHeight - contentsHeight) / 2;
+    
+    window.scrollTo({
+        top: scrollPosition,
+        behavior: 'smooth'
+    });
+}
+
+function redirectToChangePreventers() {
+    window.location.href = "../views/change-preventers.html";
+}
+
+function redirectToDispensables() {
+    window.location.href = "../views/dispensables.html";
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const menu = document.querySelector('.menu');
+
+    menuToggle.addEventListener('click', function() {
+        menu.classList.toggle('show-menu');
+    });
+});
